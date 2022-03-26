fc0c3c3b24bc0d1f2eca441383e8e55b890d92f3
Mamang touthang <huebart6@gmail.com>
2022-03-26 16:13:49 +0530

    Initial commit

---
+++ src/introducingClasses/simpleClass/BoxDemo.java
@@ -0,0 +1,28 @@
+package introducingClasses.simpleClass;
+
+// The Box class
+class Box{
+    double width;
+    double height;
+    double depth;
+}
+
+// This class declares an object of types Box.
+public class BoxDemo {
+    public static void main(String args[]){
+        Box myBox = new Box();
+        double vol;
+
+        //Assign value to myBox's in instance variables
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        //Compute volume of the box
+        vol = myBox.width * myBox.height * myBox.depth;
+
+        System.out.println("volume is " + vol);
+    }
+}
+
+
