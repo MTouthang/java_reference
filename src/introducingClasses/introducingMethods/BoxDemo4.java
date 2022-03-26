83da54898c4e4b3ba991b2c4f446e48f662ca5c0
Mamang touthang <huebart6@gmail.com>
2022-03-26 17:06:14 +0530

    adding return type to method

--- src/introducingClasses/introducingMethods/BoxDemo4.java
+++ src/introducingClasses/introducingMethods/BoxDemo4.java
@@ -1,21 +1,21 @@
 package introducingClasses.introducingMethods;
 
-class Box{
+class Box2 {
     double width;
     double height;
     double depth;
 
     //display volume of a box method
-    void volume(){
-        System.out.println("volume is ");
-        System.out.println(width*height*depth);
+    double volume(){
+      return width*height*depth;
     }
 }
 
-public class BoxDemo3 {
+public class BoxDemo4 {
     public static void main(String args[]){
         Box2 myBox12 = new Box2();
         Box2 myBox22 = new Box2();
+        double vol;
 
         //assign values to myBox1's instance variable
         myBox12.width=10;
@@ -27,11 +27,14 @@ public class BoxDemo3 {
         myBox22.height=6;
         myBox22.depth=9;
 
-        //display volume of first box
-        myBox12.volume();
+        // get volume of the first box
+        vol = myBox12.volume();
+        System.out.println("Volume of the first box " + vol);
 
-        //display volume of second box
-        myBox22.volume();
+        // get volume of the second box
+        vol = myBox22.volume();
+        System.out.println("Volume of the second box" + vol);
+
 
     }
 }
