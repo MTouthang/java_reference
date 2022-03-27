2049d795f851a6468dd0b1427615555b0bacf71d
Mamang touthang <huebart6@gmail.com>
2022-03-27 15:26:22 +0530

    returning an objects

---
+++ src/methodandclass/RetOb.java
@@ -0,0 +1,29 @@
+package methodandclass;
+
+class TestReturnObj{
+    int a;
+
+    TestReturnObj(int i){
+        a = i;
+    }
+    TestReturnObj incrByTen(){
+        TestReturnObj temp = new TestReturnObj(a+10);
+        return temp;
+    }
+
+}
+
+
+class RetOb {
+    public static void main(String[] args){
+        TestReturnObj obj = new TestReturnObj(2);
+        TestReturnObj obj2;
+
+        obj2 = obj.incrByTen();
+        System.out.println("obj1.a :" + obj.a);
+        System.out.println("obj2.a :" + obj2.a);
+
+        obj2 = obj2.incrByTen();
+        System.out.println("obj2.a after the second increased: " +obj2.a);
+    }
+}
