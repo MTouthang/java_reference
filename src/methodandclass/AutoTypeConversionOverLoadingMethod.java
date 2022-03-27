6f5e9262b9c43f9e409c16a238ecc77db743f071
Mamang touthang <huebart6@gmail.com>
2022-03-27 12:48:25 +0530

    Automatic type conversion in Method overloading

---
+++ src/methodandclass/AutoTypeConversionOverLoadingMethod.java
@@ -0,0 +1,35 @@
+package methodandclass;
+
+// Automatic type conversions apply to overloading
+class OverloadDemo{
+    void Test(){
+        System.out.println("No parameters");
+    }
+    void Test(int a){
+        System.out.println("This is Test(integer) " +a);
+    }
+
+    // Overloading test for two Integer
+    void Test(int a, int b){
+        System.out.println("a and b: " +a +" and " +b);
+    }
+
+    // Overloading test for double integer
+    void Test(double a){
+        System.out.println("Inside test(double): "+a);
+    }
+
+}
+
+class AutoTypeConversionOverLoadingMethod {
+    public static void main(String[] args){
+        OverloadDemo overObj = new OverloadDemo();
+        int i = 88;
+
+        overObj.Test();
+        overObj.Test(10, 20);
+
+        overObj.Test(i); // This will invoke Test (integer)
+        overObj.Test(123.1); // This will invoke Test (double)
+    }
+}
