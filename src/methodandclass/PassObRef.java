6683a09e6c17300edff0da98764747f6a9cc8425
Mamang touthang <huebart6@gmail.com>
2022-03-27 15:02:19 +0530

    pass by reference : class

---
+++ src/methodandclass/PassObRef.java
@@ -0,0 +1,27 @@
+package methodandclass;
+
+class Test2{
+    int a, b;
+
+    Test2(int i, int j){
+        a = i;
+        b = j;
+    }
+
+    void meth(Test2 obj){
+        obj.a *= 2;
+        obj.b /= 2;
+    }
+}
+
+class PassObRef {
+    public static void main(String[] args){
+        Test2 obj = new Test2(15, 20);
+
+        System.out.println("obj.a and obj.b before call "+ obj.a +" and " + obj.b);
+
+        obj.meth(obj);
+        System.out.println("obj.a and obj.b after call "+ obj.a +" and " + obj.b);
+
+    }
+}
