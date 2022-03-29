4c358b14fa4d3242b982adde34ec7a162ba672b8
Mamang touthang <huebart6@gmail.com>
2022-03-29 13:36:43 +0530

    simple array

---
+++ src/methodandclass/arrayrevisited/Length.java
@@ -0,0 +1,13 @@
+package methodandclass.arrayrevisited;
+
+class Length {
+    public static void main(String [] args){
+        int[] a1 = new int[10];
+        int[] a2 = {1,2,3,4,5,5,5,-1};
+        int[] a3 = {4, 3, 2, 1};
+
+        System.out.println("The length of a1: "+ a1.length);
+        System.out.println("The length of a2: " + a2.length);
+        System.out.println("The length of a3: " + a3.length);
+    }
+}
